718b59c39c7aef968346b776ce009869604e9fd7
bobyin <bobyin22@gmail.com>
2025-10-13 22:45:14 +0800

    [add] Contains Duplicate, Valid Anagram

---
+++ neetcode.playground/Pages/Design Dynamic Array(Resizable).xcplaygroundpage/Contents.swift
@@ -0,0 +1,32 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//class Solution {
+//    func containsDuplicate(_ nums: [Int]) -> Bool {
+//        var arr: [Int] = []
+//        
+//        for num in nums {
+//            if arr.contains(num) {
+//                return true
+//            }
+//            arr.append(num)
+//        }
+//        return false
+//    }
+//}
+//
+//let solution = Solution()
+//solution.containsDuplicate([1,2,3,1])
+//solution.containsDuplicate([1, 2, 2, 3])
+//solution.containsDuplicate([1, 2, 3, 4])
+//solution.containsDuplicate([1, 3, 2, 1])
+/*
+ Example 1:
+ Input: nums = [1,2,3,1]
+ Output: true
+ Explanation:
+ The element 1 occurs at the indices 0 and 3.
+ */
+
+//: [Next](@next)
