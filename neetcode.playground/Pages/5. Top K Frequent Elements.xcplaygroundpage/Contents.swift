aaa742bbe12a34f08798901ae3b872c6d9ea9223
bobyin <bobyin22@gmail.com>
2025-10-31 22:50:04 +0800

    [modify] Top K Frequent Elements_3

--- neetcode.playground/Pages/5. Top K Frequent Elements.xcplaygroundpage/Contents.swift
+++ neetcode.playground/Pages/5. Top K Frequent Elements.xcplaygroundpage/Contents.swift
@@ -2,19 +2,32 @@
 
 import Foundation
 
+/*
+ Input: nums = [1,2,2,3,3,3], k = 2
+
+ Output: [2,3]
+ */
+
 class Solution {
     func topKFrequent(_ nums: [Int], _ k: Int) -> [Int] {
         var result:[Int: Int] = [:]
-        var count = 0
         
-        nums.sort()
+        var numsArray = nums
+        numsArray.sort()
+        
+        var count = 0
+        var key = nums.first ?? 0
         // 依據 k 去查看陣列 第1多...到第k多 [3:3, 2:2, 1:1] 數字3 出現3次
         // [key , 次數]
-        nums.map {
-            let key = $0
-            count += 1
-            result[key] = count
-            print(result)
+        numsArray.map {
+            if key == $0 {
+                count += 1
+                result[key] = count
+            } else {
+                count = 1
+                result[key] = count
+            }
+            //print(result)
         }
         print("final", result)
 
